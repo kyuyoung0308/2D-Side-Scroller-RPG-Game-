c3c08dd99a0169541b4d1d8093b7657402676cc9
khan67 <khan67@uwo.ca>
2020-03-19 16:16:16 -0400

    Added boundaries for Main Camera and Player

---
+++ Hero 1/Assets/__Scripts/Boundary.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    public Transform target;
+
+    //zero out velocity
+    Vector3 velocity = Vector3.zero;
+
+    //enable/set max x
+    public bool XMaxEnabled = false;
+    public float XMaxValue = 0;
+
+    //enable/set min x
+    public bool XMinEnabled = false;
+    public float XMinValue = 0;
+
+    private void FixedUpdate()
+    {
+        //target position
+        Vector3 targetPos = target.position;
+        //horizontal
+        if (XMinEnabled && XMaxEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, XMaxValue);
+        }
+        else if (XMinEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, target.position.x);
+        }
+        else if (XMaxEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, target.position.x, XMaxValue);
+        }
+
+
+
+
+
+
+
+        //align the camera and the target z position
+        targetPos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, 0f);
+    }
+}
---
+++ Hero 1/Assets/__Scripts/FollowPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+
+    //zero out velocity
+    Vector3 velocity = Vector3.zero;
+
+    public float smoothTime = .15f;
+
+    //enable/ set max y
+    public bool YMaxEnabled = false;
+    public float YMaxValue = 0;
+
+    //enable/set  min y value
+    public bool YMinEnabled = false;
+    public float YMinValue = 0;
+
+    //enable/set max x
+    public bool XMaxEnabled = false;
+    public float XMaxValue = 0;
+
+    //enable/set min x
+    public bool XMinEnabled = false;
+    public float XMinValue = 0;
+
+    private void FixedUpdate()
+    {
+        //target position
+        Vector3 targetPos = target.position;
+
+        //vertical
+        if (YMinEnabled && YMaxEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, YMaxValue);
+        }
+        else if (YMinEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, target.position.y);
+        }
+        else if (YMaxEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y, target.position.y, YMaxValue);
+        }
+        //horizontal
+        if (XMinEnabled && XMaxEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, XMaxValue);
+        }
+        else if (XMinEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, target.position.x);
+        }
+        else if (XMaxEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, target.position.x, XMaxValue);
+        }
+
+
+
+
+
+
+
+        //align the camera and the target z position
+        targetPos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
